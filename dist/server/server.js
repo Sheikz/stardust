d6a4780ca2e7381adeacd6ea3b055a3373b984c8
Sheikz <thomulus@hotmail.com>
2016-12-31 11:53:48 +0100

    first commit

---
+++ dist/server/server.js
@@ -0,0 +1,12 @@
+"use strict";
+const express = require("express");
+var app = express();
+var value = 0;
+app.use(express.static('public'));
+app.get('/api/test', function (req, res) {
+    res.send({ value: value += 2 });
+    console.log("a");
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
